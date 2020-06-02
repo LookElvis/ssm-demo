cd6403533960fea307f346ea073a53b88b1636d8
liuxiang <504866487@qq.com>
2020-06-02 12:18:59 +0800

    update sql

--- src/main/java/com/hnu/elvis/SsmDemoApplication.java
+++ src/main/java/com/hnu/elvis/SsmDemoApplication.java
@@ -10,10 +10,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement   //开启事务管理
 @MapperScan("com.hnu.elvis.mapper")//与dao层的@Mapper二选一，会将包中接口都注解成Mapper。
-public class SsmDesApplication {
+public class SsmDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JewelsApplication.class, args);
+		SpringApplication.run(SsmDemoApplication.class, args);
 	}
 
 }
